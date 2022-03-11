cce710781a24c1383b2e643eabe51bc854038f38
flavianitto <flavia.nitto@unesp.br>
2022-03-11 07:56:00 -0300

    promise passando função no then

--- funções/promise01.js
+++ funções/promise01.js
@@ -2,12 +2,16 @@ let p = new Promise(function(resolve) {
     resolve(['Ana', 'Bia', 'Carlos', 'Daniel']);
 });
 
+// criando as functions separadamente pra só passar como parametro no then
+const primeiroElemento = arrayOuString => arrayOuString[0]; //podemos reaproveitar no segundo then
+const minuscula = letra => letra.toLowerCase();
 
 // then é para cumprir a promessa - posso chamar varias then com as functions dentro de cada uma
 // o resultado é passado para o próximo then como parametro 
 // promises retornam apenas UM valor - pode ser do tipo que eu quiser
+
 p
-    .then(primeiroElemento = array => array[0])    
-    .then(primeiraLetra = nome => nome[0])
-    .then(minuscula = letra => letra.toLowerCase())
+    .then(primeiroElemento)    
+    .then(primeiroElemento)
+    .then(minuscula)
     .then(console.log)
