21a47e348926c9eeaa6845d3be602afb8a15b88f
flavianitto <flavia.nitto@unesp.br>
2022-03-15 08:53:14 -0300

    tratamento de erros com try-catch

--- funções/promise05.js
+++ funções/promise05.js
@@ -1,11 +1,17 @@
 // tratamento de erro - reject e try catch
 function funcionarOuNao(valor, chanceErro) {
     return new Promise((resolve, reject) => {
-        if(Math.random() < chanceErro) {
-            reject('Ocorreu um erro!');
+        try {
+            console.loggg('Testando comando errado...');
+            if(Math.random() < chanceErro) {
+                reject('Ocorreu um erro!');
+            }
+            else {
+                resolve(valor);
+            }
         }
-        else {
-            resolve(valor);
+        catch (e) {
+            reject(e); //vai cair no primeiro tratamento de erro da funcionarOuNao() que ele encontrar
         }
     });
 }
@@ -15,10 +21,11 @@ funcionarOuNao('Funcionou', 0.8)
     .then(v => console.log(v))
     .then(
         () => console.lo('Sera que funciona?'),
-        err => console.log(`Erro dentro do then: ${err}`) //erro especifico do then - o catch de fora não é chamado
+        err => console.log(`Erro dentro do then: ${err}`) 
+        //erro especifico do then - o catch de fora não é chamado
     )
     .then(() => console.log('Quase fim'))
-    .catch(err => console.log(`Erro: ${err}`)) 
+    .catch(err => console.log(`Erro Geral: ${err}`)) 
     .then(() => console.log('Fim'))
 
 // geralmente colocamos o catch no final - qualquer erro cai nele
